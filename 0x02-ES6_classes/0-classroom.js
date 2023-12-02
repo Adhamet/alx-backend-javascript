b1a7811f9f9b01d9326ba884a10c8646fdf08276
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-02 13:50:14 +0200

    task 0, class ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * @param {Number} maxStudentsSize - max number of
+   * students in class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,4 @@
+# alx-backend-javascript
+
+0x01-ES6_classes
+Playing with the Symbol, Reflect & Proxy!
