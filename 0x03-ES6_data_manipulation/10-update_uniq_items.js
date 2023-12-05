16e9efefb1d586a407f445d6f9d7d27cb62ed5ca
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-12-05 13:40:43 +0200

    task 10, set map val

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+  for (const [key, value] of map) if (value === 1) map.set(key, 100);
+};
+
+export default updateUniqueItems;
